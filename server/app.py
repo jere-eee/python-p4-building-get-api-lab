24924109e849ac0df6c50e7dc87a5d130b9ad259
Akanle Jeremy <akanlejeremy@gmail.com>
2025-01-15 15:08:12 +0300

    more api building

--- server/app.py
+++ server/app.py
@@ -20,19 +20,39 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = [bakery.to_dict() for bakery in  Bakery.query.all()]
+    response = make_response(
+        all_bakeries,
+        200
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    response = make_response(
+        bakery.to_dict(),
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [good.to_dict() for good in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    response = make_response(
+        baked_goods,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    response = make_response(
+        expensive_good.to_dict(),
+        200
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
